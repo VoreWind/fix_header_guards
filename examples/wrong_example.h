0bbb043b3af63606e219362a651003a1a6b9cdb9
WereWind <serg-ef@mail.ru>
2018-03-23 10:47:56 +0300

    Add examples to use program on

---
+++ examples/wrong_example.h
@@ -0,0 +1,16 @@
+#ifndef WRONGEXAMPLE_H
+#define WRONGEXAMPLE_H
+
+#include <QString>
+
+class SomeClass {
+
+public:
+  static int Foo(int herp, bool derp);
+
+private:
+  static bool Bar(const QString &gloop);
+};
+
+#endif  // WRONGEXAMPLE_H
+
