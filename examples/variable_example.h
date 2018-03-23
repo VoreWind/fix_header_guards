ba71618a67723159b42511e0de82738aa840a0c2
WereWind <serg-ef@mail.ru>
2018-03-23 11:46:47 +0300

    Add one more example

---
+++ examples/variable_example.h
@@ -0,0 +1,16 @@
+#ifndef VARIABLE_EXAMPLE_H
+#define VARIABLE_EXAMPLE_H
+
+#include <QString>
+
+class SomeClass {
+
+public:
+  static int Foo(int herp, bool derp);
+
+private:
+  static bool Bar(const QString &gloop);
+};
+
+#endif  // VARIABLEEXAMPLE_H
+
